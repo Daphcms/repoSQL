2f99906806eaa22be60139798ceba3bdd93590ca
SARA <cancinosara39@gmail.com>
2025-04-12 12:30:08 -0600

    consulta join

--- consulta join funcional.sql
+++ consulta join funcional.sql
@@ -4,3 +4,9 @@ SELECT Carrera.Nombre,Profesor.Nombre
 FROM Carrera
 JOIN Profesor
  ON Profesor.ProfesorId = Carrera.CarreraId;
+
+CREATE TABLE Profesor(
+    ProfesorId INT AUTO_INCREMENT,
+    Nombre VARCHAR (50),
+    PRIMARY KEY (ProfesorId)
+);
